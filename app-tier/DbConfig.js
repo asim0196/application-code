d6f7739fe978186646be5c3bff5d04f53c822b3a
asim <asim@gmail.com>
2023-11-04 16:46:53 +0530

    firstcommit

---
+++ app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1-instance-1.cpdg1vv8uj7o.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'admin123',
+    DB_DATABASE : 'webappdb'
+});
